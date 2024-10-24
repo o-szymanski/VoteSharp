9905ae4d705d55a3490a8cb390a7b41aff116c9c
o-szymanski <osk.szymanski.portale@gmail.com>
2024-10-24 10:45:26 +0200

    Add Empty ASP .NET Core MVC Project

---
+++ VoteSharp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VoteSharp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
